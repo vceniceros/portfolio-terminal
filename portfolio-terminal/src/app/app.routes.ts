8e44314bf46545b4c94880caf38c780e91777ff7
vceniceros <vceniceros2001@gmail.com>
2025-12-22 17:06:05 -0300

    refactor: se cambia el proyecto a angular

---
+++ portfolio-terminal/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { Main } from './components/pages/main/main';
+
+export const routes: Routes = [
+ {
+    path: '',
+    component: Main
+ }    
+];
